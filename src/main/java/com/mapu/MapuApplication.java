d5c52ed387f9a3f2d5c2f758036c9dca28e3c856
kang yeon joo <rkdduswndi@naver.com>
2024-06-25 21:59:07 +0900

    feat: init project

---
+++ src/main/java/com/mapu/MapuApplication.java
@@ -0,0 +1,13 @@
+package com.mapu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapu/MapuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
