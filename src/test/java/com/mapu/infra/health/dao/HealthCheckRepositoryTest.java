ea36808bc0f08b4d35c841503b8412605b7a4b72
kang yeon joo <rkdduswndi@naver.com>
2024-06-29 00:00:09 +0900

    test: healthCheck 테스트코드 작성

---
+++ src/test/java/com/mapu/infra/health/dao/HealthCheckRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.mapu.infra.health.dao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestPropertySource(locations = "classpath:application.yml")
+@DataJpaTest
+class HealthCheckRepositoryTest {
+    @Autowired
+    HealthCheckRepository healthCheckRepository;
+
+    @DisplayName("DB 초기화 후, 전체 행 개수를 조회하면 4이어야 한다.")
+    @Test
+    void countAllResult3AfterInit(){
+        //when
+        long count = healthCheckRepository.count();
+        //then
+        assertThat(count).isEqualTo(4);
+    }
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO health_check (healthcheck_id, age, name) VALUES (0, 24, '강연주');
+INSERT INTO health_check (healthcheck_id, age, name) VALUES (1, 24, '김주혜');
+INSERT INTO health_check (healthcheck_id, age, name) VALUES (2, 25, '이윤정');
+INSERT INTO health_check (healthcheck_id, age, name) VALUES (3, 24, '정소은');
