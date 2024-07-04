8dec74b110ffd5af653aeb73bfc219565cb97c8b
kang yeon joo <rkdduswndi@naver.com>
2024-07-04 16:09:50 +0900

    fix: 시간 설정

--- src/main/java/com/mapu/MapuApplication.java
+++ src/main/java/com/mapu/MapuApplication.java
@@ -3,10 +3,13 @@ package com.mapu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MapuApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(MapuApplication.class, args);
 		System.out.println("Hello, MAPU :)");
 	}
