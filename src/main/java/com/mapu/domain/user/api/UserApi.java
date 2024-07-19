7cfcead5eeed7870ef2d476d618190b99e21ada1
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-07-19 16:49:47 +0900

    feat: send jwt token as a cookie after signup

--- src/main/java/com/mapu/domain/user/api/UserApi.java
+++ src/main/java/com/mapu/domain/user/api/UserApi.java
@@ -5,6 +5,9 @@ import com.mapu.domain.user.application.UserService;
 import com.mapu.domain.user.application.response.SignUpResponseDTO;
 import com.mapu.domain.user.application.response.UserDTO;
 import com.mapu.global.common.response.BaseResponse;
+import com.mapu.infra.oauth.jwt.JwtUtil;
+import com.mapu.infra.oauth.jwt.dto.JwtUserDto;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +18,25 @@ import org.springframework.web.bind.annotation.*;
 public class UserApi {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/signup")
-    public BaseResponse<SignUpResponseDTO> registerUser(@RequestBody @Validated SignUpRequestDTO signUpRequestDTO) {
+    public BaseResponse<SignUpResponseDTO> registerUser(@RequestBody @Validated SignUpRequestDTO signUpRequestDTO,
+                                                        HttpServletResponse response) {
         SignUpResponseDTO signUpResponseDTO = userService.signUp(signUpRequestDTO);
 
 
 
-        //JWT Token 반환 해주세요
+        //TODO: 유저 role, email 값 받아와서 넣기
+        String role = "ROLE_USER"; //임시 role
+        String email = "email"; //임시 이메일
+
+        JwtUserDto jwtUserDto = new JwtUserDto();
+        jwtUserDto.setName(email);
+        jwtUserDto.setRole(role);
+        response.addCookie(jwtUtil.createAccessJwtCookie(jwtUserDto));
+        response.addCookie(jwtUtil.createRefreshJwtCookie(jwtUserDto));
+
         return new BaseResponse<>(signUpResponseDTO);
     }
 }
