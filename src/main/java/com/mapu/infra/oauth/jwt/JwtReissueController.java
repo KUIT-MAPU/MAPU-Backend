8799d1370d1a1e0a75f55cbd8fe711f2b063b4cc
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-07-19 22:13:12 +0900

    fix: delete ReissueResponseDto and use string as response data

--- src/main/java/com/mapu/infra/oauth/jwt/JwtReissueController.java
+++ src/main/java/com/mapu/infra/oauth/jwt/JwtReissueController.java
@@ -4,7 +4,6 @@ import com.mapu.global.common.exception.BaseException;
 import com.mapu.global.common.exception.errorcode.BaseExceptionErrorCode;
 import com.mapu.global.common.response.BaseResponse;
 import com.mapu.infra.oauth.jwt.dto.JwtUserDto;
-import com.mapu.infra.oauth.jwt.dto.ReissueResponseDto;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,7 +20,7 @@ public class JwtReissueController {
     private final JwtService jwtService;
 
     @PostMapping("/reissue")
-    public BaseResponse<ReissueResponseDto> reissue(HttpServletRequest request, HttpServletResponse response) {
+    public BaseResponse<String> reissue(HttpServletRequest request, HttpServletResponse response) {
         String refresh = null;
         Cookie[] cookies = request.getCookies();
         if (cookies == null) {
@@ -38,6 +37,6 @@ public class JwtReissueController {
         //TODO blacklist old refresh token
         response.addCookie(jwtUtil.createRefreshJwtCookie(jwtUserDto));
 
-        return new BaseResponse<>(new ReissueResponseDto()); //TODO 응답 뭐 넣을지 채우기
+        return new BaseResponse<>("access token 재발급 성공");
     }
 }
--- src/main/java/com/mapu/infra/oauth/jwt/dto/ReissueResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.mapu.infra.oauth.jwt.dto;
-
-public class ReissueResponseDto {
-}
