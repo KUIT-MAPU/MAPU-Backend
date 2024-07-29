e901f0619ec86ad5cbcaf5759986adeb19b644c2
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-29 14:22:15 +0900

    feat: Follow Exception

---
+++ src/main/java/com/mapu/domain/follow/exception/handler/FollowExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.mapu.domain.follow.exception.handler;
+
+import com.mapu.domain.follow.exception.FollowException;
+import com.mapu.global.common.response.BaseErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class FollowExceptionHandler {
+    @ExceptionHandler(value = {FollowException.class})
+    public BaseErrorResponse handleException(FollowException e) {
+        log.error("[UserException: handle_UserException 호출]",e);
+        return new BaseErrorResponse(e.getExceptionStatus(),e.getMessage());
+    }
+}
