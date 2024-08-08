2252ddad4b28beb8cb85fcd28195761a4667bee6
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-08-08 12:19:13 +0900

    feat: 추천 에디터 조회

---
+++ src/main/java/com/mapu/domain/map/application/response/EditorListResponseDTO.java
@@ -0,0 +1,29 @@
+package com.mapu.domain.map.application.response;
+
+import com.mapu.domain.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class EditorListResponseDTO {
+    private Long id;
+    private String image;
+    private String nickname;
+    private String profileId;
+
+
+    public static EditorListResponseDTO from(User user) {
+        return EditorListResponseDTO.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+                .image(user.getImage())
+                .profileId(user.getProfileId())
+                .build();
+    }
+}
+
