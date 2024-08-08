633d1b14aad08a19a40a76293b012f87ceb692f5
kang yeon joo <rkdduswndi@naver.com>
2024-08-08 17:43:29 +0900

    feat: cors 설정

---
+++ src/main/java/com/mapu/global/common/config/CorsMvcConfig.java
@@ -0,0 +1,18 @@
+package com.mapu.global.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:9000",
+                        "https://mapu-frontend.vercel.app"
+                );
+    }
+}
